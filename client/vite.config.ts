25c22072d45d4bbc03f9cd3235ec9d3f19f7118e
Ivan Caviedes <ivancaviedes99@gmail.com>
2023-02-28 16:15:12 -0500

    configuracion e iniciacion del proyecto

---
+++ client/vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tsconfigPath from "vite-tsconfig-paths";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(),tsconfigPath()],
+})
