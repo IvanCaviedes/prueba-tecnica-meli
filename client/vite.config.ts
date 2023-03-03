e3fc116a356ef3114919188610fb8c93416ebc1e
Ivan Caviedes <ivancaviedes99@gmail.com>
2023-03-03 16:52:41 -0500

    cambiando configuracion

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tsconfigPath from "vite-tsconfig-paths";
+import tsconfigPath from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),tsconfigPath()],
+	plugins: [react(), tsconfigPath()],
 })
