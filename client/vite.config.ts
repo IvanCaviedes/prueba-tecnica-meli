19806860a39c2406ac593949098a81201c15696f
Ivan Caviedes <ivancaviedes99@gmail.com>
2023-03-06 13:19:42 -0500

    fix test

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,9 @@
-/// <reference types="vitest" />
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPath from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), tsconfigPath()],
-	test: {
-		environment: 'happy-dom'
-	}
+	plugins: [react(), tsconfigPath()]
 })
